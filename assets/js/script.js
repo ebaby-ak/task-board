801f69d56c6f98e2957c1bbcaf311a3ca5a30170
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-06-17 08:10:16 -0500

    added modal to html

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+  return nextId++;
+}
+
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+  return `<div class="card" draggable="true" data-task-id="${task.id}">
+        <div class="card-body">
+            <h5 class="card-title">${task.title}</h5>
+            <p class="card-text">${task.description}</p>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Due: ${task.dueDate}</small>
+        </div>
+    </div>`;
+}
+
+// Todo: create a function to render the task list and make cards draggable
+function renderTaskList() {}
+
+// Todo: create a function to handle adding a new task
+function handleAddTask(event) {}
+
+// Todo: create a function to handle deleting a task
+function handleDeleteTask(event) {}
+
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {}
+
+// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {});
